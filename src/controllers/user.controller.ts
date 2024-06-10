e6d4bd7d3cb728e5f9cb2166b0e8ca67f5582d2b
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-06-10 20:31:59 +0600

    create user api

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import User from "../models/user.model";
+
+export const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const { auth0Id } = req.body;
+    const existingUser = await User.findById({ auth0Id });
+
+    if (existingUser) {
+      return res.status(200).send();
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    res.status(500).json({message: "Error creating user"});
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import cors from "cors";
 import "dotenv/config";
 import express, { Application } from "express";
 import { connectDb } from "./config/db.config";
-
+import myUserRoute from "./routes/user.route"
 // express app
 const app: Application = express();
 
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/my/users', myUserRoute);
 
 // db connection
 connectDb()
