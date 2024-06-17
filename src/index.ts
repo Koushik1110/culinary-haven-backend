136ba43232edee8185e8f969055722c681a08dc0
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-06-17 12:59:14 +0600

    ready for production

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 import "dotenv/config";
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { connectDb } from "./config/db.config";
 import myUserRoute from "./routes/user.route";
 
@@ -10,6 +10,10 @@ const app: Application = express();
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.status(200).json({ message: "Welcome to our server!😊" });
+});
 
 app.use("/api/my/user", myUserRoute);
 
