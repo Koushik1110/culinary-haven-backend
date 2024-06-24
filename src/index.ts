a0cf34848a2874e8703f9b90564416e8bbeec613
Koushik1110 <148125270+Koushik1110@users.noreply.github.com>
2024-06-24 23:13:54 +0600

    create restaurant api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import cors from "cors";
 import "dotenv/config";
 import express, { Application, Request, Response } from "express";
-import { connectDb } from "./config/db.config";
-import myUserRoute from "./routes/user.route";
 import { connectCloudinary } from "./config/cloudinary.config";
+import { connectDb } from "./config/db.config";
+import myRestaurantRoute from "./routes/restaurant.route";
+import myUserRoute from "./routes/user.route";
 
 const app: Application = express();
 
@@ -11,10 +12,11 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/health", async (req: Request, res: Response) => {
-  res.status(200).json({ message: "Server is running Flawlessly👌" });
+  res.status(200).json({ message: "OK!" });
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 connectDb();
 connectCloudinary();
@@ -22,5 +24,5 @@ connectCloudinary();
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port} 👌`);
+  console.log(`Server is running on port: ${port}`);
 });
